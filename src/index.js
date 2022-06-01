20aff75594b4d937135b2d3ec5bae68e8dd0e3dd
nsharma-uk <n.sharmauk711@gmail.com>
2022-06-01 20:54:23 +0100

    questions array complete

--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
-console.log("hello from node");
-
 // 1. import inquirer
 const inquirer = require("inquirer");
 
-// 2. declare your questions array
-const questions = [
-  {
-    type: "input",
-    name: "fullName",
-    message: "Please enter is your full name:",
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "Please enter is your email address:",
-  },
-
-];
---
+++ src/questions.js
@@ -0,0 +1,64 @@
+// declare  questions array
+
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please enter a brief description of your project:",
+  },
+
+  {
+    type: "input",
+    name: "installation",
+    message: "What are the installation instructions for your project?",
+  },
+
+  
+  {
+    type: "input",
+    name: "usage",
+    message: "Please enter your project usage information:",
+  },
+
+
+  {
+    type: "list",
+    name: "license",
+    message: "Please select the license you used for this project:",
+    choices: ["Mozilla", "MIT", "Apache", "Boost"],
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Please enter your project contribution guidelines:",
+  },
+
+  {
+    type: "input",
+    name: "test",
+    message: "Please enter any testing procedures for your project:", 
+  },
+  {
+    type: "input",
+    name: "userName",
+    message: "What is your GitHub username?",
+},
+
+{
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+},
+ ];
+
+ console.log(questions);
+
+ //export questions
+module.exports = {
+  questions: questions,
+};
