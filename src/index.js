884269713715f525dd79e5047146086aa7ee0d51
nsharma-uk <n.sharmauk711@gmail.com>
2022-06-01 21:13:15 +0100

    import questions into index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-// 1. import inquirer
+// Variable importing inquirer
 const inquirer = require("inquirer");
 
+// Variable importing array of questions from questions.js
+const questions = require("./questions");
+const fs = require("fs");
