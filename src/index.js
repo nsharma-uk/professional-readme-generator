1d5a5393e66adcc75103e681c48bfbd71dd206ca
nsharma-uk <n.sharmauk711@gmail.com>
2022-06-03 10:33:45 +0100

    comments edited in index.js file

--- src/index.js
+++ src/index.js
@@ -11,15 +11,15 @@ const generateMarkdown = require("./generate-markdown");
 //Variable importing fs modules
 const fs = require("fs");
 
-// // Creating a function to initialize questions
+// Creating a function to initialize questions
   const init = async () => {
 
-// //   //prompt the questions and get answers
+//prompt the questions and get answers
   const answers = await inquirer.prompt(questions);
 
-// const readme=generateMarkdown(answers);
-console.log (answers);
-// //
+const readme=generateMarkdown(answers);
+
+ 
 fs.writeFileSync("./GENERATED-README.md", readme);
  };
 
